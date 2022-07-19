b07bff59cdbcec2d41dbb9ad0d788a99939e4514
leong1993 <ng259741@gmail.com>
2022-07-19 10:03:35 +0800

    javascript-exercises

---
+++ javascript-exercises/08_calculator/calculator.js
@@ -0,0 +1,38 @@
+const add = function(...arg) {
+  let total = 0;
+  // arg.forEach(number => total += number);
+  for (let number of arg) {
+    total += number;
+  }
+  return total;
+};
+
+const subtract = function() {
+	
+};
+
+const sum = function() {
+	
+};
+
+const multiply = function() {
+
+};
+
+const power = function() {
+	
+};
+
+const factorial = function() {
+	
+};
+
+// Do not edit below this line
+module.exports = {
+  add,
+  subtract,
+  sum,
+  multiply,
+  power,
+  factorial
+};
